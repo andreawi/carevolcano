9def25db06eb5107fb2b6849011029aa4feedf6c
andreawi <andrea.wist@gmail.com>
2018-02-06 21:14:33 +0100

    working_app

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -2,33 +2,29 @@
 var webpack = require('webpack');
 var path = require('path');
 
-//var parentDir = path.join(__dirname, '../');
-
+var parentDir = path.join(__dirname, '../');
 
 module.exports = {
     entry: [
-        path.join(__dirname, '../index.js')
+        path.join(parentDir, 'index.js')
     ],
     module: {
         loaders: [{
             test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        },{
-            test: /\.less$/,
-            loaders: ["style-loader", "css-loader", "less-loader"]
-        }, 
-                 ]
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loder", "less-loader"]
+            }
+        ]
     },
     output: {
-        path: __dirname + '/dist',
+        path: parentDir + '/dist',
         filename: 'bundle.js'
-    }, 
-
+    },
     devServer: {
-        contentBase: __dirname + '/dist',
+        contentBase: parentDir,
         historyApiFallback: true
     }
 }
-
-
