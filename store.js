0de87a4139db3ad0ff69df659da50110fd171973
Anna Selvåg Braadland <selvaag.braadland@gmail.com>
2018-02-26 20:34:22 +0100

    Redux tutorial

---
+++ store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import messageReducer from './reducers/message'
+import thunk from 'redux-thunk'
+
+const reducer = combineReducers({
+ messageReducer
+})
+
+const store = createStore(
+ reducer,
+ applyMiddleware(thunk)
+)
+
+export default store;
